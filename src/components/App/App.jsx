3b2727ef7d0f6614c542b2eb651d7369e21f5a24
Denis-Ozolin <ozolindenx@gmail.com>
2021-10-06 23:44:43 +0300

    copy version with hooks

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,99 +1,67 @@
 import { v4 as uuidv4 } from 'uuid';
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { ContactForm } from 'components/ContactForm/ContactForm';
 import { ContactList } from 'components/ContactList/ContactList';
 import { Filter } from 'components/Filter/Filter';
 import { Container, AppTitle } from './App.styled';
 
-export class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
+export function App() {
+  const defaultContacts = [
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    { id: 'id-5', name: 'Baraq Obama', number: '342-34-22' },
+    { id: 'id-6', name: 'John Veek', number: '456-45-64' },
+    { id: 'id-7', name: 'Gerald Wicher', number: '666-66-66' },
+  ];
+  const [contacts, setContacts] = useState(
+    () => JSON.parse(localStorage.getItem('contacts')) || defaultContacts,
+  );
+  const [filter, setFilter] = useState('');
 
-  componentDidMount() {
-    const contacts = localStorage.getItem('contacts');
-    const parsedContacts = JSON.parse(contacts);
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
-    if (parsedContacts) {
-      this.setState({
-        contacts: parsedContacts,
-      });
-    }
-  }
-
-  componentDidUpdate(_, prevState) {
-    const { contacts } = this.state;
-
-    if (contacts.length !== prevState.contacts.length) {
-      localStorage.setItem('contacts', JSON.stringify(contacts));
-    }
-  }
-
-  formSubmitHandler = ({ name, number }) => {
-    this.onCheckUniqueName(name)
+  const formSubmitHandler = (name, number) => {
+    onCheckUniqueName(name)
       ? alert(`${name} is alredy in contacts.`)
-      : this.setState(({ contacts }) => ({
-          contacts: [
-            {
-              id: uuidv4(),
-              name,
-              number,
-            },
-            ...contacts,
-          ],
-        }));
+      : setContacts(state => [
+          {
+            id: uuidv4(),
+            name,
+            number,
+          },
+          ...state,
+        ]);
   };
 
-  filterUpdateHandler = data => {
-    this.setState({
-      filter: data,
-    });
-  };
-
-  onCheckUniqueName = value => {
-    const { contacts } = this.state;
-
+  const onCheckUniqueName = value => {
     return contacts.find(({ name }) => name === value);
   };
 
-  filterContactsHandler = () => {
-    const { contacts, filter } = this.state;
-
+  const filterContactsHandler = () => {
     return contacts.filter(({ name }) =>
       name.toLocaleLowerCase().includes(filter.toLocaleLowerCase()),
     );
   };
 
-  onDeleteContact = id => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== id),
-    }));
+  const onDeleteContact = id => {
+    setContacts(state => state.filter(contact => contact.id !== id));
   };
 
-  render() {
-    const { contacts, filter } = this.state;
-
-    return (
-      <Container>
-        <AppTitle>Phonebook</AppTitle>
-        <ContactForm onSubmit={this.formSubmitHandler} />
-        <Filter onGetValue={this.filterUpdateHandler} />
-        {!filter ? (
-          <ContactList title="Contacts" contacts={contacts} onDelete={this.onDeleteContact} />
+  return (
+    <Container>
+      <AppTitle>Phonebook</AppTitle>
+      <ContactForm onSubmit={formSubmitHandler} />
+      <Filter onGetValue={setFilter} />
+      {contacts.length > 0 &&
+        (!filter ? (
+          <ContactList title="Contacts" contacts={contacts} onDelete={onDeleteContact} />
         ) : (
-          <ContactList
-            title="Searched contacts"
-            contacts={this.filterContactsHandler()}
-            onDelete={this.onDeleteContact}
-          />
-        )}
-      </Container>
-    );
-  }
+          <ContactList title="Searched contacts" contacts={filterContactsHandler()} />
+        ))}
+    </Container>
+  );
 }
